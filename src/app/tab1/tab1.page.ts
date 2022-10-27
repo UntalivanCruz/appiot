ff3a457ecf887bd8a3cce79834a4d9afb3a55bc7
Ivan Cruz <ivancruzhn@gmail.com>
2022-10-27 09:39:04 -0600

    actualizacion pantalla inicio y acerca

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  slides: { img: string, titulo: string, desc: string }[] = [
+    {
+      img: '/assets/compu.jpg',
+      titulo: 'Bienvenido',
+      desc: 'Sistema de monitoreo'
+    },
+    {
+      img: '/assets/Ima2_preview_rev_1.png',
+      titulo: '¿Que es IIOT?',
+      desc: 'Es el conjunto de sensores, instrumentos y dispositivos autónomos conectados a través de Internet a aplicaciones industriales.'
+    },
+  ];
 
+  constructor( private navCtrl: NavController) {}
+
+  onClick(){
+    this.navCtrl.navigateBack('tabs/tab2');
+  }
 }
